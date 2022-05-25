e5457343c4e680a4d5bd162433766281d921425c
lavillerose <melyz3@hotmail.com>
2022-05-25 02:25:47 +0200

    Modification attribut classe.py et ajout getter pour bdd et lecture

--- classe.py
+++ classe.py
@@ -1,9 +1,8 @@
 class test():
     def _init(self):
-        self.name="Alice"
-        self.ville="cix"
-        self.age=11
+        self.name="Bob"
+        iself.age=11
         self.get_bdd="file"
         self.read_bdd=True
-        print("helo")
+    
 
